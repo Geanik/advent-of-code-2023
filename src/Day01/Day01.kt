43a24612c5e9e3ea09941b0e3bc875733dd0a585
Geanik <Geanik@users.noreply.github.com>
2023-11-30 20:41:56 +0100

    Add Day01 from last year for an example test run

--- src/Day01/Day01.kt
+++ src/Day01/Day01.kt
@@ -1,15 +1,41 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var maxCalorieSum = 0
+        var curCalorieSum = 0
+
+        for (curCalories in input.map { it.toIntOrNull() }) {
+            if (curCalories != null) {
+                curCalorieSum += curCalories
+            } else {
+                maxCalorieSum = maxOf(maxCalorieSum, curCalorieSum)
+                curCalorieSum = 0
+            }
+        }
+        maxCalorieSum = maxOf(maxCalorieSum, curCalorieSum)
+
+        return maxCalorieSum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var curCalorieSum = 0
+        val calSums = mutableListOf<Int>()
+
+        for (curCalories in input.map { it.toIntOrNull() }) {
+            if (curCalories != null) {
+                curCalorieSum += curCalories
+            } else {
+                calSums.add(curCalorieSum)
+                curCalorieSum = 0
+            }
+        }
+        calSums.add(curCalorieSum)
+
+        return calSums.sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput(name = "Day01", testInput = true)
-    check(part1(testInput) == 2)
+    check(part1(testInput) == 24000)
 
     val input = readInput(name = "Day01")
     part1(input).println()
